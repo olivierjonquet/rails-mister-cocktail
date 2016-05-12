d795e3d326e84c45e9521eeae9c5ddca33f8dc84
olivierjonquet <jonqueto@hotmail.fr>
2016-05-12 11:27:47 +0200

    valdiation models

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ActiveRecord::Base
+  belongs_to :ingredient
+  belongs_to :cocktail
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail_id, :uniqueness => {:scope => :ingredient_id}
+end
